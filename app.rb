353422f648464f9d04d79b25931112fdef5342f1
Axel06 <Anisvan@mail.ru>
2021-04-03 19:50:10 +0600

    add rakefile

--- app.rb
+++ app.rb
@@ -2,7 +2,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require "sinatra/activerecord"
+
+set :database, {adapter: "sqlite3", database: "barbershop.db"}
+
+class Client < ActiveRecord::Base
+end
+
+class Barber < ActiveRecord::Base
+end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index	
 end
